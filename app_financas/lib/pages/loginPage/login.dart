2686eda26de4c1be992b08a81d506935670f1d94
RyanMelo <ryangarciamelo@gmail.com>
2022-05-18 12:16:06 -0300

    Alterações no form de login

--- app_financas/lib/pages/loginPage/login.dart
+++ app_financas/lib/pages/loginPage/login.dart
@@ -9,132 +9,101 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
-  bool _marcado = false;
-
   void funcaoTeste() {
-   print('clicou');
+    debugPrint("clicou");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Theme.of(context).colorScheme.secondary,
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              children:  [
-                Text(
-                  "Bem \nVindo!",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.w200,
-                    fontSize: 48,
-                    decoration: TextDecoration.none
+      body: SingleChildScrollView(
+        child: Container(
+          color: Theme.of(context).colorScheme.secondary,
+          padding: const EdgeInsets.all(20),
+          child: Form(
+            // key: formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Bem\nVindo!",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.w200,
+                          fontSize: 48,
+                          decoration: TextDecoration.none
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      const Image(
+                        image: AssetImage('assets/imgs/graficos.png'),
+                        width: 180,
+                      )
+                    ],
                   ),
                 ),
-                const SizedBox(width: 20),
-                const Image(
-                  image: AssetImage('assets/imgs/graficos.png'),
-                  width: 200,
-                )
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: TextFormField(
+                    // controller: email,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: TextFormField(
+                    // controller: senha,
+                    obscureText: true,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Senha',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.only(left: 147, right: 147, top: 15, bottom: 15)
+                    ),
+                    onPressed: funcaoTeste,
+                    child: const Text(
+                      "Login"
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    "ou",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    "Cadastrar-se",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  )
+                ),
               ],
-            ),
-            const SizedBox(height: 40),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10.0),
-                  )
-                ),
-                fillColor: Colors.red,
-                labelText: 'Seu Email',
-                labelStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
-                ),
-              ),
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.primary,                fontSize: 18,
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10.0),
-                  )
-                ),
-                fillColor: Colors.red,
-                labelText: 'Senha',
-                labelStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
-                ),
-              ),
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.primary,
-                fontSize: 18,
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: funcaoTeste, 
-              style: ElevatedButton.styleFrom(
-                primary: Theme.of(context).colorScheme.primary,
-                padding: const EdgeInsets.only(left: 135, right: 135, top: 15, bottom: 15)
-              ),
-              child: Text(
-                "Fazer Login",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-              )
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: CheckboxListTile(
-                    title: const Text("Lembrar senha"),
-                    activeColor: Theme.of(context).colorScheme.primary,
-                    value: _marcado,
-                    controlAffinity: ListTileControlAffinity.leading,
-                    onChanged: (bool? valor) {},
-                  )
-                ),
-                const Text("Esqueceu a senha ?"),
-              ],
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              "Ou",
-              style: TextStyle(
-                fontSize: 22,
-              ),
-            ),
-            const SizedBox(height: 30),
-            
-            GestureDetector(
-              child: Text(
-                "Cadastrar-se",
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Theme.of(context).colorScheme.primary,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              onTap: () {
-                Navigator.pushNamed(context, '/criarConta');
-              },
-            ),
-          ],
+            )
+          )
         ),
       ),
     );
